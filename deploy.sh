7afd1351402ba8ee1c608f8abbf5ba2d7048f065
shin5ok <shin5ok@55mp.com>
2023-04-05 20:07:26 +0900

    be able to take arg such as specific project

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcloud beta run deploy --set-env-vars=BITLY_APIKEY=$BITLY_APIKEY --source=. --platform=managed --region=asia-northeast1 --allow-unauthenticated qrcode-redirect
+gcloud run deploy --set-env-vars=BITLY_APIKEY=$BITLY_APIKEY --source=. --platform=managed --region=asia-northeast1 --allow-unauthenticated qrcode-redirect $@
